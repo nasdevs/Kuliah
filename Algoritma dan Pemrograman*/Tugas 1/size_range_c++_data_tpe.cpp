04c3a5c374469171b67e8a05446e70ed0aed6b19
Nas Devs <89463001+nasdevs@users.noreply.github.com>
2021-09-19 16:54:02 +0800

    Create size_range_c++_data_tpe.cpp

---
+++ Algoritma dan Pemrograman*/Tugas 1/size_range_c++_data_tpe.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <limits>
+#include <float.h>
+#include <wchar.h>
+using namespace std;
+// Nasrullah, github.com/nasdevs
+int main() {
+   cout << "========================================" << endl;
+   cout << "=====Ukuran dan Jangkauan Tipe Data=====" << endl;
+   cout << "========================================" << endl;
+   cout << "char" << endl;
+   cout << "\tUkuran : " << sizeof(char) << endl;
+   cout << "\tJangkauan : " << CHAR_MIN << " - " << CHAR_MAX << endl;
+   cout << "----------------------------------------" << endl;
+   cout << "unsgined char" << endl;
+   cout << "\tUkuran : " << sizeof(char) << endl;
+   cout << "\tJangkauan : " << 0 << " - " << UCHAR_MAX << endl;
+   cout << "----------------------------------------" << endl;
+   cout << "int" << endl;
+   cout << "\tUkuran : " << sizeof(int) << endl;
+   cout << "\tJangkauan : " << INT_MIN << " - " << INT_MAX << endl;
+   cout << "----------------------------------------" << endl;
+   cout << "short int" << endl;
+   cout << "\tUkuran : " << sizeof(short int) << endl;
+   cout << "\tJangkauan : " << SHRT_MIN << " - " << SHRT_MAX << endl;
+   cout << "----------------------------------------" << endl;
+   cout << "usigned int" << endl;
+   cout << "\tUkuran : " << sizeof(unsigned int) << endl;
+   cout << "\tJangkauan : " << 0 << " - " << UINT_MAX << endl;
+   cout << "----------------------------------------" << endl;
+   cout << "long int" << endl;
+   cout << "\tUkuran : " << sizeof(long) << endl;
+   cout << "\tJangkauan : " << LONG_MIN << " - " << LONG_MAX << endl;
+   cout << "----------------------------------------" << endl;
+   cout << "usingned long int" << endl;
+   cout << "\tUkuran : "  << sizeof(unsigned long int) << endl;
+   cout << "\tJangkauan : " << 0 << " - " << ULONG_MAX << endl;
+   cout << "----------------------------------------" << endl;
+   cout << "float" << endl;
+   cout << "\tUkuran : " << sizeof(float) << endl;
+   cout << "\tJangkauan : " << FLT_MIN << " to " << FLT_MAX << endl;
+   cout << "----------------------------------------" << endl;
+   cout << "double" << endl;
+   cout << "\tUkuran : " << sizeof(double) << endl;
+   cout << "\tJangkauan : " << DBL_MIN << " - " << DBL_MAX << endl;
+   cout << "----------------------------------------" << endl;
+   cout << "wchar_t" << endl;
+   cout << "\tUkuran : " << sizeof(wchar_t) << endl;
+   cout << "\tJangkauan : " << WCHAR_MIN << " - " << WCHAR_MAX << endl;
+   cout << "----------------------------------------" << endl;
+   
+   return 0 ;
+}
