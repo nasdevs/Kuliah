a39e3f35ea528bd6ac3b3ff4a40528340d11d1e5
Nas Devs <89463001+nasdevs@users.noreply.github.com>
2021-10-25 14:53:20 +0800

    Create if.cpp

---
+++ algoritma-dan-pemrograman-1/tugas-3/if.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+// Nasrullah, github.com/nasdevs
+int main() {
+    long long int nim;
+    char cekDataMahasiswa;
+    cout << "Pengecekan nim mahasiswa UMI" << endl;
+    cout << "Masukkan nim : "; cin >> nim;
+
+    if (nim == 13020210134) {
+        cout << "Memang benar bahwa mahasiswa dengan nim : " << nim << " adalah mahasiswa UMI" << endl;
+        cout << "Mau cek data lengkapnya [Y/N] : "; cin >> cekDataMahasiswa;
+        cout << endl;
+        if (cekDataMahasiswa == 'Y' || cekDataMahasiswa == 'y') {
+            cout << "Nama        : Nasrullah" << endl;
+            cout << "Nim         : 13020210134" << endl;
+            cout << "Tahun masuk : 2021" << endl;
+            cout << "Domisili    : Makassar" << endl;
+            cout << "Nomor telp. : 082122215109" << endl;
+        }
+    }
+    
+    return 0;
+}
